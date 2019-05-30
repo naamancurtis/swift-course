2113a6200506cffb0fe84318fa27a12d1031517d
Naaman Curtis <naaman.the.dev@gmail.com>
2019-05-30 06:23:53 +0100

    Pushing Section 1 & Section 2 of the course

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Naaman Curtis on 29/05/2019.
+//  Copyright © 2019 Naaman Curtis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Naaman Curtis on 30/05/2019.
+//  Copyright © 2019 Naaman Curtis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
