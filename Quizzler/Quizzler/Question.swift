851de28181c8a8bac3072cad74261332b2258faa
Naaman Curtis <naaman.the.dev@gmail.com>
2019-06-02 16:26:00 +0100

    Adding Quizzler App - Section 9 - Model View Controller

---
+++ Quizzler/Quizzler/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Naaman Curtis on 02/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    let questionText: String
+    let answer: Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+}
