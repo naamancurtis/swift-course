ac84be739f6331174984eaf3677b04ec755cf088
Naaman Curtis <naaman.the.dev@gmail.com>
2019-06-03 16:45:00 +0100

    Uploading auto-layout section, including StackViews

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Naaman Curtis on 03/06/2019.
+//  Copyright © 2019 Naaman Curtis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
