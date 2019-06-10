002d59734ce7c90c8951d945bf1c7cf47e2e32e6
Naaman Curtis <naaman.the.dev@gmail.com>
2019-06-10 05:49:42 +0100

    Updating Section 17 - Flash Chat

---
+++ Flash Chat/Flash Chat/NewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewTableViewCell.swift
+//  Flash Chat
+//
+//  Created by Naaman Curtis on 09/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class NewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
